b312f73c0ea67614e6533a871cd89244e73154da
Firly Arie Azland <arieazland@gmail.com>
2021-06-27 07:47:57 +0700

    Update urlconfig.js

--- urlconfig.js
+++ urlconfig.js
@@ -1,3 +1,16 @@
-var MAIN_URL = 'http://localhost:8080';
+var os = require('os');
+var MAIN_URL = null;
+
+if(os.hostname().indexOf("local") > -1){
+    var MAIN_URL = 'http://localhost:5025';
+}else{
+    var MAIN_URL = '';
+}
+
+
+/** 
+ * Server address:
+ * var MAIN_URL = 'http://localhost:8080';
+*/
 
 module.exports = MAIN_URL;
