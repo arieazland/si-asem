704460ff9e266ab3550610bcc16aa83473824515
Firly Arie Azland <arieazland@gmail.com>
2021-07-15 18:54:22 +0700

    add disable button login and register

---
+++ public/js/client/disablebutton/login.js
@@ -0,0 +1,25 @@
+function Disable(idform, idbutton1, idbutton2){
+    $(document).ready(function () {
+
+        $('#'+idform).submit(function (e) {
+
+            //stop submitting the form to see the disabled button effect
+            // e.preventDefault();
+
+            //disable the submit button
+            $('#'+idbutton1).attr("disabled", true);
+
+            //disable a rest button
+            $('#'+idbutton2).attr("disabled", true);
+
+            return true;
+
+        });
+    });
+}
+
+// form tambah user
+var idform = "loginform";
+var idbutton1 = "tombollogin";
+var idbutton2 = "tombolreset";
+Disable(idform, idbutton1, idbutton2);
