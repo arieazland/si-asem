97076be594eecf0cb08101ba3677b451610e98e1
Firly Arie Azland <arieazland@gmail.com>
2021-08-25 18:27:08 +0700

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -59,7 +59,7 @@ Router.get('/', async (req, res) => {
         fakultas = req.session.fakultas
         prodi = req.session.prodi
         ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-        ipadd = ip.split(':')[1]
+        ipadd = ip.substr(7)
         if(tipe === 'mahasiswa'){
             /** login page di arahkan ke page user */
             // res.render("indexmahasiswa",{
